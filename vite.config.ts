13735a049645d6c5bbae1f6f9369ea6027bcea09
Max V. Shumakov <shumakovmv@gmail.com>
2026-03-15 23:08:38 +0300

    remove debug code

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,9 @@
-// import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import electron from 'vite-plugin-electron/simple'
 import path from 'node:path'
 
-// https://vite.dev/config/
 export default defineConfig({
   base: './',
   plugins: [
@@ -26,7 +23,6 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      // '@': fileURLToPath(new URL('./src', import.meta.url))
       '@': path.resolve(__dirname, './src')
     },
   },
